6b0d93f774f2491a139a195468ba0328fdb61f43
gara.matt <gara.matt@86d6501e-b178-3a45-4bb8-06cafb282295>
2010-06-17 22:53:59 +0000

    Completed first Neural Network implementation. Tested neural network with XOR, works well as long as over-fitting is not done.
    I found that a linear activation function in 
    the output layer gives much better final 
    results.

--- src/nn.cpp
+++ src/nn.cpp
@@ -1,36 +1,75 @@
 #include "nn.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
 
+#define RMAX  97451
 
-double dotprod( double *w, double *x, int nx )
+////////////////////////////////////////////////////////
+// Performs the dot product of input neurons with weigh-
+// ts.
+////////////////////////////////////////////////////////
+inline double dotprod( double *w, neuron_t **x, int nx )
 {
 	double res = 0;
 	for ( int i = 0; i < nx; i++ )
 	{
-		res = w[i] * x[i];
+		res += w[i] * x[i]->v;
 	}
 	return res;
 }
 
 void free_neuron( neuron_t *n )
 {
-	delete[] n->w;
-	delete[] n->x;
-	delete[] n->out;
+		if ( n->nx > 0 )
+		{
+				delete[] n->w;
+				delete[] n->x;
+		}
+		if ( n->ny > 0 )
+		{
+				delete[] n->y;
+		}
 }
 
 void init_neuron( neuron_t *n,
 	       	int nx,
-		int nout	)
+		int ny	)
 {
 	n->nx = nx;
-	n->nout = nout;
-	n->w = new double[nx];
-	n->x = new neuron_t*[nx];
-	n->out = new neuron_t*[nout];
+	n->ny = ny;
+	if ( nx > 0 )
+	{
+			n->w = new double[nx];
+			n->x = new neuron_t*[nx];
+
+			// init weights to random numbers
+			for ( int i = 0; i < nx; i++ )
+			{
+					n->w[i] = (double)( rand() % RMAX) / (double)(RMAX - 1 );
+			}
+	}
+	else
+	{
+			n->w = 0;
+			n->x = 0;
+	}
+	if ( ny > 0 )
+	{
+			n->y = new neuron_t*[ny];
+	}
+	else
+	{
+			n->y = 0;
+	}
 }
 
 void nn_free( neural_network_t *nn )
 {
+	for ( int i = 0; i < nn->nil; i++ )
+	{
+		free_neuron( &nn->il[i] );
+	}
 	for ( int i = 0; i < nn->nhl; i++ )
 	{
 		free_neuron( &nn->hl[i] );
@@ -41,7 +80,7 @@ void nn_free( neural_network_t *nn )
 	}
 	delete[] nn->hl;
 	delete[] nn->ol;
-	delete[] nn->input;
+	delete[] nn->il;
 }
 
 
@@ -52,25 +91,41 @@ void nn_init( int ninput,
 		double (*hidden_actp)(double),
 		double (*output_act)(double),
 		double (*output_actp)(double),
+		double learnrate,
 	       	neural_network_t *out_nn )
 {
-	out_nn->ninput = ninput;
+	out_nn->nil = ninput;
 	out_nn->nhl = nhidden;
 	out_nn->nol = noutput;
 
+	out_nn->il = new neuron_t[ninput];
 	out_nn->hl = new neuron_t[nhidden];
 	out_nn->ol = new neuron_t[noutput];
-	out_nn->input = new double[ninput];
+
+	out_nn->learn_rate = learnrate;
 
 	neuron_t *n1,*n2;
 
 	
+	// TODO: currently bias is always 0,
+	// may want to change this in the
+	// future
+	for ( int i = 0; i < ninput; i++ )
+	{
+		n1 = &out_nn->il[i];
+		init_neuron( n1, 0, nhidden ); 
+		// do not specify an activation
+		n1->act =  0;
+		n1->actp = 0;
+		n1->b = 0;
+	}
 	for ( int i = 0; i < nhidden; i++ )
 	{
 		n1 = &out_nn->hl[i];
 		init_neuron( n1, ninput, noutput ); 
 		n1->act =  hidden_act;
 		n1->actp = hidden_actp;
+		n1->b = 0;
 	}
 	for ( int i = 0; i < noutput; i++ )
 	{
@@ -78,19 +133,38 @@ void nn_init( int ninput,
 		init_neuron( n1, nhidden, 0  ); 
 		n1->act = output_act;
 		n1->actp = output_actp;
+		n1->b = 0;
 	}
 
 
 	// hook up all the neurons
 
+	for ( int i = 0; i < ninput; i++ )
+	{
+		n1 = &out_nn->il[i];
+		for ( int j = 0; j < nhidden; j++ )
+		{
+			n2 = &out_nn->hl[j];
+
+			n1->y[j] = n2;
+		}
+	}	
+
+
 	for ( int i = 0; i < nhidden; i++ )
 	{
 		n1 = &out_nn->hl[i];
+		for ( int j = 0; j < ninput; j++ )
+		{
+			n2 = &out_nn->il[j];
+
+			n1->x[j] = n2;
+		}
 		for ( int j = 0; j < noutput; j++ )
 		{
 			n2 = &out_nn->ol[j];
 
-			n1->out[j] = n2;
+			n1->y[j] = n2;
 		}
 	}	
 
@@ -107,10 +181,134 @@ void nn_init( int ninput,
 
 }
 
+void eval_neuron( neuron_t *n )
+{
+		// dot product the weights
+		// with inputs
+		if ( n->nx > 0 )
+		{
+				n->a = dotprod( n->w,
+								n->x,
+								n->nx );
+		}
+		else
+		{
+				n->a = 0;
+		}
+
+		// pass through activation
+		// function
+		if ( n->act )
+		{
+				n->v =  n->act(n->a + n->b);
+		}
+		else
+		{
+				n->v = 0;
+		}
+
+
+}
+
 
 void nn_eval( neural_network_t *nn )
 {
+		// do nothing with input layer!
+		
+		for( int i = 0; i < nn->nhl; i++ )
+		{
+				eval_neuron( &nn->hl[i] );
+		}
 
+		for( int i = 0; i < nn->nol; i++ )
+		{
+				eval_neuron( &nn->ol[i] );
+		}
 }
+
+inline double compute_output_delta( neuron_t *on, double t )
+{
+#ifdef DEBUG
+	//return (  t - on->v ) * on->v * ( 1 - on->v );
+	if ( fabs( on->v * ( 1- on->v ) - on->actp( on->a ) ) > .00000001 )
+	{
+			printf( "error1!\n" );
+	}
+#endif
+	return (  t - on->v ) * on->actp( on->a  );
+}
+
+inline double compute_hidden_delta( neuron_t *hn, int hidden_index )
+{
+		double res;
+		neuron_t *n;
+		
+		res = 0;
+		for ( int i = 0; i < hn->ny; i++ )
+		{
+				n = hn->y[i];
+				res += n->d * n->w[hidden_index];
+		}
+
+
+#ifdef DEBUG
+	if ( fabs( hn->v * ( 1- hn->v ) - hn->actp( hn->a ) ) > .000000001 )
+	{
+			printf( "error2!\n" );
+	}
+#endif
+		res *= hn->actp( hn->a );//hn->v * ( 1 - hn->v );
+
+		return res;
+}
+
+
+void nn_backpropagate( neural_network_t *nn,
+											 double *desired_output,
+											 int nout )
+{
+		if ( nout != nn->nol )
+		{
+				fprintf( stderr, "err: number of desired output "
+												"do not match actual number of "
+												"output in the nn\n" );
+				exit( -1 );
+		}
+
+		// calculate the deltas
+		for( int i = 0; i < nn->nol; i++ )
+		{
+				nn->ol[i].d = compute_output_delta( &nn->ol[i],
+								desired_output[i] );
+		}
+		for ( int i = 0; i < nn->nhl; i++ )
+		{
+				nn->hl[i].d = compute_hidden_delta( &nn->hl[i], i );
+		}
+		
+
+
+		// update weights using deltas
+		neuron_t *n;
+		for( int i = 0; i < nn->nol; i++ )
+		{
+				n = &nn->ol[i];
+				for ( int j = 0; j < n->nx; j++ )
+				{
+						//printf( "---- w=%g u=%g\n", n->w[j], nn->learn_rate * n->d * n->x[j]->v );
+						n->w[j] += nn->learn_rate * n->d * n->x[j]->v;
+
+				}
+		}
+		for( int i = 0; i < nn->nhl; i++ )
+		{
+				n = &nn->hl[i];
+				for ( int j = 0; j < n->nx; j++ )
+				{
+						n->w[j] += nn->learn_rate * n->d * n->x[j]->v;
+				}
+		}
+}
+
 
 
